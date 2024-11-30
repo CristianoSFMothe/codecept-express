e3601f0e7a8494e567ab56a0d7b8c0ef168815ee
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-11-30 20:40:29 -0300

    feat: task duplication validation

--- e2e/tasks_test.js
+++ e2e/tasks_test.js
@@ -3,13 +3,33 @@ Feature('tasks')
 Scenario('deve poder cadastrar uma nova tarefa',  ({ I }) => {
   const tasksName = 'Estudar JavaScript'
 
-  I.sendDeleteRequest('http://localhost:3333/helper/tasks/' + tasksName)
+  I.sendDeleteRequest('/helper/tasks/' + tasksName)
   I.seeResponseCodeIsSuccessful()
 
   I.amOnPage('/')
   I.fillField('input[placeholder$=Task]', tasksName)
   I.click('Create')
   I.see(tasksName, '.task-item')
+
+  I.wait(3)
+})
+
+Scenario.only('não deve cadastrar tarefas com nome duplicado', ({ I }) => {
+  const task = {
+    name: 'Estudar Automação',
+    is_done: false,
+  }
+
+  I.sendDeleteRequest('/helper/tasks/' + task.name)
+  I.seeResponseCodeIsSuccessful()
+
+  I.sendPostRequest('/tasks/', task)
+  I.seeResponseCodeIsSuccessful()
+
+  I.amOnPage('/')
+  I.fillField('input[placeholder$=Task]', task.name)
+  I.click('Create')
+  // I.see('Task already exists!', '.swal2-html-container')
 
   I.wait(3)
 })
